23a2a051dd53aa1dd30d1c12891e8fcdecdeaa27
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2020-02-24 16:44:27 +0100

    both js and css compile

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,48 @@
 const autoprefixer = require('autoprefixer');
-module.exports = [{
-    entry: './app.scss',
-    output: {
-      // This is necessary for webpack to compile
-      // But we never use style-bundle.js
-      filename: 'style-bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: 'bundle.css',
+
+module.exports = {
+  entry: ['./app.scss', './app.js'],
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          {loader: 'extract-loader'},
+          {loader: 'css-loader'},
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+              sassOptions: {
+                includePaths: ['./node_modules'],
               },
             },
-            { loader: 'extract-loader' },
-            { loader: 'css-loader' },
-            {
-                loader: 'postcss-loader',
-                options: {
-                   plugins: () => [autoprefixer()]
-                }
-              },
-            {
-              loader: 'sass-loader',
-              options: {
-                // Prefer Dart Sass
-                implementation: require('sass'),
-                sassOptions: {
-                    includePaths: ['./node_modules']
-                  },
-              },
-            },
-          ]
-        }
-      ]
-    },
-  }];
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+        },
+      }
+    ],
+  },
+};
